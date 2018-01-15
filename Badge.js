9f2ad52d03c63db2424a4a692d4ff9b5bd1aa4cb
Hagen Hübel <hhuebel@itinance.com>
2018-01-15 10:18:39 +0100

    Fixed PropTypes

--- Badge.js
+++ Badge.js
@@ -7,8 +7,11 @@
 
 import React, {
     Component,
-    PropTypes,
 } from 'react'
+
+import PropTypes from 'prop-types'
+import ViewPropTypes from 'react-native';
+
 import {
     View,
     Text,
@@ -45,8 +48,7 @@ export default class Badge extends Component {
     static propTypes = {
         //borderRadius: PropTypes.number,   //number 18, null 5
         extraPaddingHorizontal: PropTypes.number,
-        style: View.propTypes.style,
-        textStyle: Text.propTypes.style,
+        style: ViewPropTypes.style,
         minHeight: PropTypes.number,
         minWidth: PropTypes.number,
     }
